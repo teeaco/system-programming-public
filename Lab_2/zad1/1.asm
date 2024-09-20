cd5fb19bfb67351c2e344f1212b01be7d2131e72
teeaco <151190454+teeaco@users.noreply.github.com>
2024-09-20 09:49:43 +0000

    I did it!!

---
+++ Lab_2/zad1/1.asm
@@ -0,0 +1,37 @@
+format ELF64
+public _start
+public exit
+
+section '.bss' writable
+my db 0xA, "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
+section '.text' executable
+_start:
+  mov rcx, 52
+  .iter:
+    push rcx
+
+    mov rax, my
+    add rax, rcx
+    mov rcx, rax 
+
+    mov rax, 4
+    mov rbx, 1
+
+    mov rdx, 1
+    int 0x80
+
+    pop rcx
+
+    dec rcx
+    cmp rcx, -1
+    jne .iter
+
+
+  call exit
+
+
+exit:
+  mov rax, 1
+  xor rbx, rbx
+  int 0x80
